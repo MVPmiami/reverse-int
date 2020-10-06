8ad8115f1086ffb6ea3c915a14746be647b38dfb
Aliaksei Siankevich <andfly@tut.by>
2020-10-06 19:09:44 +0300

    feat: add body function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
+  let result = '';
+  if(n < 0){
+    n = n * (-1);
+    let numArray = n.toString().split('');
+    numArray.reverse();
+    numArray.forEach(function(item) {
+      result += item;
+    });
+  }else{
+    let numArray = n.toString().split('');
+    numArray.reverse()
   
+    numArray.forEach(function(item) {
+      result += item;
+    });
+  }
+  return +result;
 }
